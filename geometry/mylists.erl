d7e94c03bc2927d6d955453252467fc04cbdda05
Yumm <mmyu@hanshows.com>
2015-02-25 00:35:29 +0800

    qsort算法

--- geometry/mylists.erl
+++ geometry/mylists.erl
@@ -1,5 +1,11 @@
 -module(mylists).
--export([sum/1]).
+-export([sum/1, qsort/1]).
 
 sum([H|T]) -> H + sum(T);
 sum([]) -> 0.
+
+qsort([]) -> [];
+qsort([P|T]) ->
+	qsort([X || X <- T, X < P])
+	++ [P] ++
+	qsort([X || X <- T, X >= P]).
