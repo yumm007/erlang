e4537524f03a3461d99ea382643cb08848622a9d
Yumm <mmyu@hanshows.com>
2015-03-03 22:56:07 +0800

    增加测试但愿

--- geometry/geometry.erl
+++ geometry/geometry.erl
@@ -1,5 +1,11 @@
 -module(geometry).
--export([area/1]).
+-export([test/0, area/1]).
+
 area({rectangle, Width, Ht}) -> Width * Ht;
 area({square, X}) -> X * X;
 area({circle, R}) -> 3.14159 * area({square, R}).
+
+test() ->
+	12 = area({rectangle, 3, 4}),
+	144 = area({square, 12}),
+	tests_worked.
